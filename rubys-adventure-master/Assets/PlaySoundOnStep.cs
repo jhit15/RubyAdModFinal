fe6048628922695b193aa910fc756939ff685beb
unknown <ja781651@ucf.edu>
2024-04-21 21:55:48 -0400

    Read Jake Hitzges readme for details about submission

---
+++ rubys-adventure-master/Assets/PlaySoundOnStep.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnStep : MonoBehaviour
+{
+
+    AudioSource source;
+    
+    Collider2D soundTrigger; 
+
+    void Awake() {
+        source = GetComponent<AudioSource>();
+        soundTrigger = GetComponent<Collider2D>();
+    }
+   
+    void OnTriggerEnter2D(Collider2D collider) {
+        source.Play();
+    }
+   
+}
